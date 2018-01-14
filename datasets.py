1c6a35d0b21e2d9083463a0afa036c0497f28619
pgulley <nano3.14@gmail.com>
2018-01-14 16:02:06 -0500

    Reconfigured datasets abstraction for a filesystem-style management, tests for those functions, and a dataset demo in jupyter

--- datasets.py
+++ datasets.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import random
+import os
 
 
 class DataSet(object):
@@ -80,32 +81,36 @@ class DataSet(object):
     else:
       return self._inputs[start:end], self._outputs[start:end]
 
-def data_singles(loc):
-	chips = []
-	labels = []
-	for i in pd.DataFrame.from_csv(loc).itertuples():
-		
-.		chips.append(i[1])
-		labels.append(i[2])
-	return DataSet(np.asarray(chips), np.asarray(labels))
 
-def train_singles():
-	return data_singles("data/train_set.csv")
 
 def test_singles():
-	return data_singles("data/test_set.csv")
+	#awake
+	for root, dirs, filenames in os.walk("data/test/awake"):
+		awake = [(np.load(os.path.join(root, file_)), 1) for file_ in filenames]
+	for root, dirs, filenames in os.walk("data/test/asleep"):
+		asleep = [(np.load(os.path.join(root, file_)), 0) for file_ in filenames]
+	
+	random.shuffle(awake)
+	merged = [j for i in zip(awake,asleep) for j in i]
+	chips, labels = zip(*merged) #zip(*) is inverse of zip()- so [(a,1), (b,2), (c,3)] -> [(a, b, c), (1, 2, 3)]
+	return DataSet(np.asarray(chips), np.asarray(labels))
 
-def data_pairs(data, num_pairs):
-	sleep = []
-	awake = []
-	for entry in data:
-		if entry[2] == 1:
-			awake.append(entry)
-		elif entry[2] == 0:
-			sleep.append(entry)
+""" #commented out because there is no train data right now
+def train_singles():
+	for root, dirs, filenames in os.walk("data/train/awake"):
+		awake = [(np.load(os.join(root, file_)), 1) for file_ in filenames]
+	for root, dirs, filenames in os.walk("data/train/asleep"):
+		asleep = [(np.load(os.join(root, file_)), 0) for file_ in filenames]
+	random.shuffle(awake)
+	merged = [j for i in zip(awake,asleep) for j in i]
+	chips, labels = zip(*merged) #zip(*) is inverse of zip()- so [(a,1), (b,2), (c,3)] -> [(a, b, c), (1, 2, 3)]
+	return DataSet(np.asarray(chips), np.asarray(labels))
+"""
 
+
+def data_pairs(awake, asleep, num_pairs):
 	#If we want repeatable experiments, we'll need to use random.seed()
-	random.shuffle(sleep)
+	random.shuffle(asleep)
 	random.shuffle(awake)
 	
 	chips = []
@@ -116,12 +121,16 @@ def data_pairs(data, num_pairs):
 		labels.append(chip[1])
 
 	for i in range(num_pairs):
-		a = sleep[i]
-		b = sleep[-i]
+		#basically we generate the pairs by popping both the first and last elements 
+		#it limits the number of unique pairs we can generate
+		#but we it's a limit on how many pairs we can create w/o repeating any singles.
+		a = asleep[i]
+		b = asleep[-i]
 		c = awake[i]
 		d = awake[-i]
 
-		
+		#we choose whether to make a "same" pair or a "different" pair
+		#with mod2 of the iteration. We add two pairs each time. final data is split 50/50 between same and diff. 
 		if i%2 == 0:
 			add_pair([[a, b], 1])
 			add_pair([[c, d], 1])
@@ -135,51 +144,41 @@ def data_pairs(data, num_pairs):
 	return DataSet(np.asarray(chips), np.asarray(labels))
 
 
-
+""" #Again, these don't exist rn.
 def train_pairs(num):
-	data = [i for i in pd.DataFrame.from_csv("data/train_set.csv").itertuples()]
-	return data_pairs(data, num)
+	for root, dirs, filenames in os.walk("data/train/awake"):
+		awake = [(np.load(os.join(root, file_)), 1) for file_ in filenames]
+	for root, dirs, filenames in os.walk("data/train/asleep"):
+		asleep = [(np.load(os.join(root, file_)), 0) for file_ in filenames]
+
+	return data_pairs(awake, asleep, num)
+"""
 
 def test_pairs(num):
-	data = [i for i in pd.DataFrame.from_csv("data/test_set.csv").itertuples()]
-	return data_pairs(data, num)
+	for root, dirs, filenames in os.walk("data/test/awake"):
+		awake = [(np.load(os.path.join(root, file_)), 1) for file_ in filenames]
+	for root, dirs, filenames in os.walk("data/test/asleep"):
+		asleep = [(np.load(os.path.join(root, file_)), 0) for file_ in filenames]
+
+	return data_pairs(awake, asleep, num)
 
 
 def tests():
-	"""
-	tests for the above. 
 
-	print "ten random train batches size 10"
+	#tests for the above functions. 
+
+	print "ten random singlet batches"
 	for t in range(10):
-		A = train_data()
+		A = test_singles()
 		for i in range(0,10):
 			print A.next_batch(10)
 
-	print "ten random test batches size 10"
-	for t in range(10):
-		A = test_data()
-		for i in range(0,10):
-			print A.next_batch(10)
-
-	print "let's just see if the randomization works"
-	A = train_data()
-	last = None
-	for t in range(100):
-		test = A.next_batch(10)
-		if last is not None:
-			if np.array_equal(test, last):
-				print "same"
-			else:
-				print "diff"	
-		last = test
    
-	"""
-	print "let's get a few data pairs"
-	A = train_pairs(1000)
+	
+	print "ten random paired batches"
+	A = test_pairs(10)
 	for i in range(10):
 		print A.next_batch(10)
-	
-	pass 
 
 
 if __name__ == "__main__":
